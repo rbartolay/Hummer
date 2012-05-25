b571d739cff7e7fba618dafb1fa96a5fbd4fe9d5
Ryan Bartolay <Ryan Bartolay@RyanBartolay-PC>
2012-05-25 19:12:31 +0800

    added primary template

--- Hummer/application/templates/default/footer.php
+++ Hummer/application/templates/default/footer.php
@@ -2,5 +2,10 @@
 	global $loading_time;	
 	echo "Page generated in " . Core_Utilities::endTimer($loading_time);
 ?>
-	</body>
-</html>
+		<div class="powered">
+		Copyright © JobBoardMount.com Job Board 2010
+		&nbsp;|&nbsp;Powered by <a href="http://www.jobboardmount.com" target="_blank">JobMount Job Board Software</a></div>
+		</div>
+	</div>
+</body>
+</html> 
